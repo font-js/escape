91a6ddcb7ebbee3bff4c8d294447b3a955f9b843
Nevermore <a919899931@gmail.com>
2015-03-07 01:31:47 +0800

    support CommonJS and AMD/CMD (untest)

--- index.js
+++ index.js
@@ -8,28 +8,51 @@
 
 'use strict';
 
-function Escape() {}
 
-Escape.version = '0.0.1';
+! function(name, definition) {
 
-Escape.prototype = {
+    var hasDefine = typeof define === 'function';
+    var hasExports = typeof module !== 'undefined' && module.exports;
 
-	/**
-	 * escape special characters in the given string of html.
-	 *
-	 * @param {String} html
-	 * @return {String}
-	 * api private
-	 */
+    if (hasDefine) {
+        // AMD or CMD module
+        if (typeof define.amd === 'object') {
+            define(name, definition);
+        } else {
+            define(definition);
+        }
+    } else if (hasExports) {
+        // CommonJS module
+        module.exports = definition();
+    } else {
+        // Broswer module
+        this[name] = definition();
+    }
 
-	html: function (html) {
-		return String(html)
-			.replace(/&/g, '&amp;')
-			.replace(/"/g, '&quot;')
-			.replace(/'/g, '&#39;')
-			.replace(/</g, '&lt;')
-			.replace(/>/g, '&gt;');
-	}
+}('escape', function() {
 
+    var Escape = function() {};
 
-};
+    Escape.version = '0.0.1';
+
+    Escape.prototype = {
+        /**
+         * escape special characters in the given string of html.
+         *
+         * @param {String} html
+         * @return {String}
+         * api private
+         */
+        html: function(html) {
+            return String(html || (typeof html !== 'undefined' ? html.toString() : ''))
+                .replace(/&/g, '&amp;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+        }
+
+    };
+
+    return Escape;
+});
